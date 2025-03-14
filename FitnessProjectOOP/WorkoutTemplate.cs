927587f899ff7fa647d13a1672e6d6d254af15ca
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2025-03-14 12:39:24 +0000

    changes to xaml, added workoutTemplate class

---
+++ FitnessProjectOOP/WorkoutTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProjectOOP
+{
+
+
+
+    public class WorkoutTemplate
+    {
+        // Workout Template Properties 
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int Sets { get; set; } // Number of sets
+        public int Reps { get; set; } // Number of repetitions per set
+        //public List<Exercise> Exercises { get; set; } 
+        // Don't have an excercise class yet 
+
+
+    }
+}
